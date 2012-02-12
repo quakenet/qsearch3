464290519ffabdc355817b71e67acef82b06e585
Chris Porter <slug@quakenet.org>
2012-02-12 00:18:20 +0000

    Use solr binary encoding to fix control codes.

--- src/main/java/org/quakenet/qsearch3/SolrIndexer.java
+++ src/main/java/org/quakenet/qsearch3/SolrIndexer.java
@@ -6,6 +6,7 @@ import org.apache.commons.httpclient.URI;
 import org.apache.commons.httpclient.URIException;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.BinaryRequestWriter;
 import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
 import org.apache.solr.common.SolrInputDocument;
 
@@ -20,6 +21,7 @@ final public class SolrIndexer {
 	public SolrIndexer(String solrBaseURL) throws MalformedURLException, URIException {
 		solrBaseURL = solrBaseURL + (solrBaseURL.endsWith("/") ? "" : "/");
 		_solr = new CommonsHttpSolrServer(solrBaseURL + "write");
+		_solr.setRequestWriter(new BinaryRequestWriter());
 		_swapURI = new URI(solrBaseURL + "admin/cores?action=SWAP&core=write&other=read", false);
 	}
 	
